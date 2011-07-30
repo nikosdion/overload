c440a967503196c1904a7d3dc0b58840dd47ecfc
Nicholas Dionysopoulos <nicholas@akeebabackup.com>
2011-07-30 16:04:12 +0300

    Adding license and README files

---
+++ component/README.php
@@ -0,0 +1,77 @@
+<?php die(); ?>
+================================================================================
+COPYRIGHT AND DISCLAIMER
+================================================================================
+
+Overload! - Mass content creator for demo and testing purposes
+Copyright (C) 2011  Nicholas K. Dionysopoulos / AkeebaBackup.com
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+The full text of the license can be found in the LICENSE.txt file inside all of
+our ZIP packages.
+
+================================================================================
+CONTACT AND SUPPORT
+================================================================================
+
+If you have further questions, you can contact the author directly by emailing
+at nicholas@akeebabackup.com. Alternatively, you can post on our free support
+forum at http://www.akeebabackup.com/forum.html or send a Twitter message to
+@akeebabackup or @nikosdion. If you wish to contact us by Instant Messaging or
+Skype voice call, please leave a Personal Message to user "nicholas" on our
+official site http://www.akeebabackup.com stating the reason of communication,
+your Skype handle and timezone so that we can arrange for such communication.
+
+================================================================================
+DOCUMENTATION AND DOWNLOADS
+================================================================================
+
+***
+This section is tentatively left blank until com_overload is migrated to a
+proper GitHub repository.
+***
+
+================================================================================
+GET INVOLVED
+================================================================================
+
+Free Software is about Freedom of Choice and collaborating with the global
+community to deliver the best software to all users. If you run into any
+problem, have written a new/improved feature you want to share with the world,
+have any suggestion/comment/wish or if you want to provide additional
+documentation or tutorials in any form (text, audio, video, etc) feel free to
+contact us by any of the available means. We take user contributions and
+requests very seriously. Everything you share back with us, even if it is a bug
+report, helps us improving our software.
+
+Get involved now!
+
+================================================================================
+CHANGELOG
+================================================================================
+
+The Changelog is provided in reverse chronological order.
+Legend: # Bug fix     + Addition     - Feature removal     ~ Change
+        ! Critical bug fix     ^ Minor edit
+
+
+Version 1.1 (July 30th, 2011)
+--------------------------------------------------------------------------------
+# Categories were not enabled by default
+# Articles were not enabled by default
+		
+Version 1.0 (July 30th, 2011)
+--------------------------------------------------------------------------------
+First version
